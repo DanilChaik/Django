5414d476e5687bebc87d61c9f7b06d8a6ab0a3ed
DanilChaik <96342751+DanilChaik@users.noreply.github.com>
2021-12-19 01:15:12 +0200

    initial commit

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'main/index.html')
 
 
-def about(request):
-    return render(request, 'main/about.html')
+def forma(request):
+    return render(request, 'main/forma.html')
